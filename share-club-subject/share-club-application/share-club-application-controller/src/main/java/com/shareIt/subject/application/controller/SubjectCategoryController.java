1d69253924721e4db828c6e165dab48963173833
liutongxun <liutx24@gmail.com>
2025-05-23 22:58:01 +0800

    chore(swagger): centralize springdoc version in parent and add dependencies; annotate controllers
    - Add `springdoc.version` property and `springdoc-openapi-ui` entry in parent POM’s `<dependencyManagement>`
    - Reference `springdoc-openapi-ui` in starter and controller modules without specifying version
    - Apply OpenAPI annotations on `SubjectCategoryController` methods for API documentation

--- share-club-subject/share-club-application/share-club-application-controller/src/main/java/com/shareIt/subject/application/controller/SubjectCategoryController.java
+++ share-club-subject/share-club-application/share-club-application-controller/src/main/java/com/shareIt/subject/application/controller/SubjectCategoryController.java
@@ -1,5 +1,14 @@
 package com.shareIt.subject.application.controller;
 
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import com.alibaba.fastjson.JSON;
 import com.google.common.base.Preconditions;
 import com.shareIt.domain.entity.SubjectCategoryBO;
@@ -7,6 +16,8 @@ import com.shareIt.domain.service.SubjectCategoryDomainService;
 import com.shareIt.subject.application.convert.SubjectCategoryDTOConverter;
 import com.shareIt.subject.application.dto.SubjectCategoryDTO;
 import com.shareIt.subject.common.entity.Result;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 /**
  * controller for classification of practice questions
@@ -22,6 +34,11 @@ import javax.annotation.Resource;
  * @author: Liu Tongxun
  * @date: 2025/05/22
  */
+
+@Tag(
+        name        = "Category Management",
+        description = "APIs for creating, querying and updating subject categories"
+)
 @RestController
 @RequestMapping("/subject/category")
 @Slf4j
@@ -30,9 +47,23 @@ public class SubjectCategoryController {
     @Resource
     private SubjectCategoryDomainService subjectCategoryDomainService;
 
+
     /**
      * create new category
      */
+    @Operation(
+            summary     = "Create a new category",
+            description = "Creates a new subject category based on the provided DTO"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description  = "Category created successfully",
+                    content      = @Content(schema = @Schema(implementation = Result.class))
+            ),
+            @ApiResponse(responseCode = "400", description = "Invalid input parameters"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping("/add")
     public Result<Boolean> add(@RequestBody SubjectCategoryDTO subjectCategoryDTO) {
 
@@ -41,13 +72,19 @@ public class SubjectCategoryController {
                 log.info("SubjectCategoryController.add.dto:{}",
                                                     JSON.toJSONString(subjectCategoryDTO));
             }
-            Preconditions.checkNotNull(subjectCategoryDTO.getCategoryType(),
-                                                                    "分类类型不能为空");
-            Preconditions.checkArgument(!StringUtils.isBlank(subjectCategoryDTO.getCategoryName()),
-                                                                    "分类名称不能为空");
-            Preconditions.checkNotNull(subjectCategoryDTO.getParentId(),
-                                                                    "分类父级id不能为空");
-
+            System.out.println(" subjectCategoryDTO.getCategoryType(),"+ subjectCategoryDTO.getCategoryType());
+            Preconditions.checkNotNull(
+                    subjectCategoryDTO.getCategoryType(),
+                    "Category type must not be null"
+            );
+            Preconditions.checkArgument(
+                    !StringUtils.isBlank(subjectCategoryDTO.getCategoryName()),
+                    "Category name must not be blank"
+            );
+            Preconditions.checkNotNull(
+                    subjectCategoryDTO.getParentId(),
+                    "Parent category ID must not be null"
+            );
             SubjectCategoryBO subjectCategoryBO = SubjectCategoryDTOConverter.
                                                     INSTANCE.
                                                     convertDtoToCategoryBO(subjectCategoryDTO);
