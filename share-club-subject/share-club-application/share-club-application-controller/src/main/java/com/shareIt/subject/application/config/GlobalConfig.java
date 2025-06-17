49a3fa6acd648b52e34b4b4d627903a22a621dc9
liutongxun <liutx24@gmail.com>
2025-06-17 16:51:15 +0800

    Configure custom Jackson message converter to ignore null values and handle empty beans

---
+++ share-club-subject/share-club-application/share-club-application-controller/src/main/java/com/shareIt/subject/application/config/GlobalConfig.java
@@ -0,0 +1,40 @@
+package com.shareIt.subject.application.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * Global MVC handling
+ *
+ * @author: Liu Tongxun
+ * @date: 2025/06/17
+ */
+@Configuration
+public class GlobalConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+
+    /**
+     * Custom mappingJackson2HttpMessageConverter
+     * Current implementation: ignore null values, return empty fields if necessary
+     */
+    private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+
+}
